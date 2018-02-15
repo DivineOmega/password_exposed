53b1c8884a5582472252592c1a92d2c3d3b3ba15
Jordan Hall <jordan.hall@rapidweb.biz>
2018-02-15 11:13:48 +0000

    Automatic rate limiting - waits 2 seconds between requests

--- src/PasswordExposedChecker.php
+++ src/PasswordExposedChecker.php
@@ -4,6 +4,7 @@ namespace DivineOmega\PasswordExposed;
 
 use GuzzleHttp\Client;
 use rapidweb\RWFileCachePSR6\CacheItemPool;
+use GuzzleHttp\Psr7\Response;
 
 class PasswordExposedChecker
 {
@@ -11,6 +12,8 @@ class PasswordExposedChecker
     private $cache;
 
     const CACHE_EXPIRY_SECONDS = 60 * 60 * 24 * 30;
+    const TIME_BETWEEN_REQUESTS_SECONDS = 2;
+    const WAIT_TIMEOUT_SECONDS = 60;
 
     public function __construct()
     {
@@ -38,21 +41,13 @@ class PasswordExposedChecker
             return $cacheItem->get();
         }
 
-        sleep(2);
+        $proceed = $this->wait();
 
-        $response = $this->makeRequest($hash);
-
-        $status = PasswordStatus::UNKNOWN;
-
-        switch ($response->getStatusCode()) {
-            case 200:
-                $status = PasswordStatus::EXPOSED;
-                break;
-
-            case 404:
-                $status = PasswordStatus::NOT_EXPOSED;
-                break;
+        if (!$proceed) {
+            return PasswordStatus::UNKNOWN;
         }
+
+        $status = $this->getPasswordStatus($this->makeRequest($hash));
 
         if (in_array($status, [PasswordStatus::EXPOSED, PasswordStatus::NOT_EXPOSED])) {
             $cacheItem->set($status);
@@ -73,5 +68,44 @@ class PasswordExposedChecker
         ];
 
         return $this->client->request('GET', 'pwnedpassword/'.$hash, $options);
+    }
+
+    private function getPasswordStatus(Response $response)
+    {
+        switch ($response->getStatusCode()) {
+            case 200:
+                return PasswordStatus::EXPOSED;
+
+            case 404:
+                return PasswordStatus::NOT_EXPOSED;
+        }
+
+        return PasswordStatus::UNKNOWN;
+    }
+
+    /**
+     * The wait method waits until 2 seconds have passed since the last request, then returns true.
+     * If we have been waiting for greater than self::WAIT_TIMEOUT_SECONDS, give up and return false.
+     */
+    private function wait()
+    {
+        $startTime = time();
+
+        while(true) {
+
+            $lastRequestCacheItem = $this->cache->getItem('last_request');
+            if (!$lastRequestCacheItem->isHit() || $lastRequestCacheItem->get() < time() - self::TIME_BETWEEN_REQUESTS_SECONDS) {
+                $lastRequestCacheItem->set(time());
+                $this->cache->save($lastRequestCacheItem);
+                return true;
+            }
+            
+            if ($startTime < time() - self::WAIT_TIMEOUT_SECONDS) {
+                return false;
+            }
+            
+            sleep(1);
+
+        }
     }
 }
