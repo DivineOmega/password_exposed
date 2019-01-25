d9a1ad12d169926d1a331bdb83e7cdaf72952743
Nikita Loges <git@loges.one>
2019-01-25 14:16:47 +0100

    remove redundancy

--- src/PasswordExposedChecker.php
+++ src/PasswordExposedChecker.php
@@ -157,19 +157,18 @@ class PasswordExposedChecker
             // If we can't write to the our Certainty data directory, just
             // use the latest bundle from the Certainty package.
             return (new Fetch($ourCertaintyDataDir))->getLatestBundle();
-        } else {
-            if (PHP_INT_SIZE === 4 && !extension_loaded('sodium')) {
-
-                // If the platform would run verification checks slowly, use the
-                // latest bundle from the Certainty package and disable verification.
-                return (new Fetch($ourCertaintyDataDir))->getLatestBundle(false, false);
-            } else {
-
-                // If the platform can run verification checks well enough, get
-                // latest remote bundle and verify it.
-                return (new RemoteFetch($ourCertaintyDataDir))->getLatestBundle();
-            }
         }
+
+        if (PHP_INT_SIZE === 4 && !extension_loaded('sodium')) {
+
+            // If the platform would run verification checks slowly, use the
+            // latest bundle from the Certainty package and disable verification.
+            return (new Fetch($ourCertaintyDataDir))->getLatestBundle(false, false);
+        }
+
+        // If the platform can run verification checks well enough, get
+        // latest remote bundle and verify it.
+        return (new RemoteFetch($ourCertaintyDataDir))->getLatestBundle();
     }
 
     /**
