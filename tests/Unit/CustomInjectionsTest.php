98d70e5d472751fe17cec592f610c893589f9321
Jordan Hall <jordan@hall05.co.uk>
2019-02-04 22:38:51 +0000

    Reorder imports

--- tests/Unit/CustomInjectionsTest.php
+++ tests/Unit/CustomInjectionsTest.php
@@ -4,8 +4,8 @@ namespace DivineOmega\PasswordExposed\Tests;
 
 use Buzz\Client\FileGetContents;
 use DivineOmega\DOFileCachePSR6\CacheItemPool;
-use DivineOmega\PasswordExposed\PasswordExposedChecker;
 use DivineOmega\PasswordExposed\Enums\PasswordStatus;
+use DivineOmega\PasswordExposed\PasswordExposedChecker;
 use Faker\Factory;
 use Http\Discovery\Psr17FactoryDiscovery;
 use ParagonIE\Certainty\Bundle;
